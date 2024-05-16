19b48c7ce82c8f15ef51aa0e86f19d44edeeef8a
dapraws <darrelprawira26@gmail.com>
2024-05-17 02:09:04 +0700

    feat: update home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,15 +5,11 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
+    return const Scaffold(
+      body: Center(
         child: Text(
           'HomeView is working',
           style: TextStyle(fontSize: 20),
