548eed9edea2ca87f700a08d003f3d1580572101
dapraws <darrelprawira26@gmail.com>
2024-05-19 18:17:37 +0700

    feat: update your asset page

--- lib/app/modules/your_asset/views/your_asset_view.dart
+++ lib/app/modules/your_asset/views/your_asset_view.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:tracky/app/data/models/asset_model.dart';
+import 'package:tracky/app/shared/themes/font.dart';
+import 'package:tracky/app/shared/widgets/listviewdata_widget.dart';
+import 'package:tracky/app/shared/widgets/search_bar_widget.dart';
 
 import '../controllers/your_asset_controller.dart';
 
 class YourAssetView extends GetView<YourAssetController> {
-  const YourAssetView({Key? key}) : super(key: key);
+  const YourAssetView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('YourAssetView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'YourAssetView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Asset List',
+                style: h2Bold.copyWith(
+                  color: const Color(0xFFf29e28),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const SearchBarWidget(),
+              const SizedBox(height: 20),
+              ListViewDataWidget(listData: dummyAsset),
+            ],
+          ),
         ),
       ),
     );
